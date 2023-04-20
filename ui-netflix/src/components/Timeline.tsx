119f860620f41e79dfe2522c3335a8304e37ca1d
Alex - Gollinha <114370092+devalex16@users.noreply.github.com>
2023-04-20 19:40:01 -0300

    Update Timeline.tsx

--- ui-netflix/src/components/Timeline.tsx
+++ ui-netflix/src/components/Timeline.tsx
@@ -13,7 +13,7 @@ export default function Timeline() {
            <div key={list}>
              <h2 className="text-lg font-bold">{list}</h2>
              <div className="flex gap-4 mb-[30px] snap-mandatory snap-x overflow-y-auto w-[90vw]">
-               {movies.map(({data}:{data?:array}) => {
+               {movies.map(({data}:{data?:Array}) => {
                  return (
                   <a key={data.title} href={data.trailer} target="_blank">
                    <div className="h-[20vh] w-[27vw] snap-center relative" key={data.title}>
