7e1fbec386e1b2bc51a3da5481e92a3955355773
devalex16 <114370092+devalex16@users.noreply.github.com>
2023-04-20 18:33:36 +0000

    update

--- ui-netflix/src/components/Header.tsx
+++ ui-netflix/src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <span className="flex flex-row justify-around w-[100%] items-center relative">
       <div className="flex flex-row bg-zinc-100 w-[80%] px-5 py-2 rounded">
-        <Search className="text-zinc-500"/>
+        <Search />
         <p className="text-zinc-500 text-base px-3">Search</p>
       </div>
       <figure className="h-[80%] w-[8%] rounded relative">
